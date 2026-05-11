60326a92c38f87bdf6aab07fac60b2b123f2aa54
student <erikhag04@gmail.com>
2026-05-11 17:13:19 +0200

    Finished Snake.cpp

--- blatt-03/Snake.cpp
+++ blatt-03/Snake.cpp
@@ -34,7 +34,7 @@ void initGame() {
 void drawPixel(int row, int column, int color) {
   attron(COLOR_PAIR(color));
   attron(A_REVERSE);
-  mvprintw(row, column, "  ");
+  mvprintw(row, column * 2, "  ");
   attroff(A_REVERSE);
   attroff(COLOR_PAIR(color));
 }
@@ -47,7 +47,7 @@ void drawBorder(int color) {
   }
 
   for (int j = 0; j < dim_y; ++j) {
-    drawPixel(j, 0 color);
+    drawPixel(j, 0, color);
     drawPixel(j, dim_x - 1, color);
   }
 }
@@ -56,11 +56,13 @@ void drawBorder(int color) {
 void drawSnake(int color) { drawPixel(pos_y, pos_x, color); }
 // ___________________________________________________________________________
 bool collidesWithBorder() {
-  if (pos_x =) {
-    endwin();
+  bool collision = false;
+  if (pos_x <= 0 || pos_x >= dim_x - 1) {
+    collision = true;
+  } else if (pos_y <= dim_y || pos_y >= dim_y - 1) {
+    collision = true;
   }
-  elif (pos_x = dim_x - 1) { endwin(); }
-  elif (pos_x = dim_y - 1) { endwin(); }
+  return collision;
 }
 
 // ___________________________________________________________________________
@@ -73,13 +75,28 @@ void moveSnake() {
     pos_y += 1;
     break;
   case KEY_RIGHT:
-    pos_x += 2;
+    pos_x += 1;
     break;
   case KEY_LEFT:
-    pos_x -= 2;
+    pos_x -= 1;
     break;
   }
 }
 
 // ___________________________________________________________________________
-bool handleKey(int key) {}
+bool handleKey(int key) {
+  if (key == 27) {
+    return true;
+  } else if (key == KEY_UP && dir_pxl != KEY_DOWN) {
+    dir_pxl = KEY_UP;
+  } else if (key == KEY_DOWN && dir_pxl != KEY_UP) {
+    dir_pxl = KEY_DOWN;
+        }
+  else if (key == KEY_LEFT && dir_pxl != KEY_RIGHT {
+    dir_pxl = KEY_LEFT;
+	}
+  else if (key == KEY_RIGHT && dir_pxl != KEY_LEFT) {
+    dir_pxl = KEY_RIGHT;
+	}
+return false;
+}
