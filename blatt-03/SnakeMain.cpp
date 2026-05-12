50eefd2d4ceafdcb4ecf78f011bfcbf5182a28b1
student <erikhag04@gmail.com>
2026-05-12 09:54:24 +0200

    Update

--- blatt-03/SnakeMain.cpp
+++ blatt-03/SnakeMain.cpp
@@ -14,12 +14,34 @@ int main() {
   refresh();
 
   while (true) {
+    int sleep_time_us = 1'000'000 / speed;
+    usleep(sleep_time_us);
     int key = getch();
     if (key != ERR) {
       if (handleKey(key)) {
         break;
       }
     }
+    moveSnake();
+    if collidesWithBorder () {
+      break;
+    }
+    clear();
+    drawBooder(1);
+    drawSnake(2);
+    refresh();
+
+    speed += acceleration * (sleep_time_us / 1'000'000.0);
   }
-}
+  mvprintw(LINES / 2, (COLS / 2) - 5, "GAME OVER!");
+  mvprintw((LINES / 2) + 1, (COLS / 2) - 10, "Press ESC to exit...");
+  refresh();
+
+  while (true) {
+    if (getch() == 27) {
+      break;
+    }
+    uslepp(1000);
+    endwin();
+  }
 }
