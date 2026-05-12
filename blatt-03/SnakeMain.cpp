127c4da24dc4a2f7e8d624c97d3d791736311be0
student <erikhag04@gmail.com>
2026-05-12 09:14:00 +0200

    Update

--- blatt-03/SnakeMain.cpp
+++ blatt-03/SnakeMain.cpp
@@ -3,10 +3,26 @@
 #include <unistd.h>
 
 int main() {
-	startNcurses();
-	initTerminal();
-	initGame();
-	drawBorder();
-	drawSnake();
-	while (true) ;
-		}
+  startNcurses();
+  initTerminal();
+  initGame();
+  while (true) {
+    clear();
+    drawBorder();
+    drawSnake();
+    for (int i, i <= 10'000, ++i) {
+      int key = getch();
+      if (key != ERR) {
+        mvprintw(2, 2, "Key: %3d", key);
+        refresh();
+      }
+      if (handleKey(key) {
+        break;
+	}
+      moveSnake();
+      if (collidesWithBorder()) {
+        break;
+	}
+    }
+  }
+}
--- blatt-03/SnakeTest.cpp
+++ blatt-03/SnakeTest.cpp
@@ -7,35 +7,34 @@ int KEY_LEFT = 260;
 int KEY_RIGHT = 261;
 
 TEST(SnakeTest, initGame) {
-	dim_x = 20;
-	dim_y = 20;
-	initGame();
-	EXPECT_EQ(pos_x, 10);
-	EXPECT_EQ(pos_y, 10);
-	EXPECT_EQ(dir_pxl, KEY_RIGHT);
-	}
+  dim_x = 20;
+  dim_y = 20;
+  initGame();
+  EXPECT_EQ(pos_x, 10);
+  EXPECT_EQ(pos_y, 10);
+  EXPECT_EQ(dir_pxl, KEY_RIGHT);
+}
 
 TEST(SnakeTest, collidesWithBorder) {
-	dim_x = 20;
-	dim_y = 20;
-	pos_x = 0;
-	pos_y = 9;
-	EXPECT_TRUE(collidesWithBorder());
-	}
+  dim_x = 20;
+  dim_y = 20;
+  pos_x = 0;
+  pos_y = 9;
+  EXPECT_TRUE(collidesWithBorder());
+}
 
 TEST(SnakeTest, moveSnake) {
-	pos_x = 10;
-	pos_y = 10;
-	dir_pxl = KEY_UP;
-	moveSnake();
-	EXPECT_EQ(pos_y, 9);
-	}
+  pos_x = 10;
+  pos_y = 10;
+  dir_pxl = KEY_UP;
+  moveSnake();
+  EXPECT_EQ(pos_y, 9);
+}
 
 TEST(SnakeTest, handleKey) {
-	EXPECT_TRUE(handleKey(27));
-	
-	dir_pxl = KEY_RIGHT;
-	EXPECT_FALSE(handleKey(KEY_LEFT));
-	EXPECT_EQ(dir_pxl, KEY_RIGHT);
-	}	
+  EXPECT_TRUE(handleKey(27));
 
+  dir_pxl = KEY_RIGHT;
+  EXPECT_FALSE(handleKey(KEY_LEFT));
+  EXPECT_EQ(dir_pxl, KEY_RIGHT);
+}
