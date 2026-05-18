17f74093506cd0d150818d0d4627e6d176d504f2
student <erikhag04@gmail.com>
2026-05-18 10:19:27 +0200

    Changes

--- blatt-04/GameOfLife.cpp
+++ blatt-04/GameOfLife.cpp
@@ -1,14 +1,21 @@
-#include <GameOfLife.h>
+#include "GameOfLife.h"
 #include <ncurses.h>
 
-bool* a = actualState[];
-bool* n = nextState[];
+bool *a = actualState[];
+bool *n = nextState[];
 
+int dimX;
+int dimY;
+
+// ___________________________________________________________________________
+const int MAX_NUM_CELLS = 10'000;
+bool actualState[MAX_NUM_CELLS] = {0};
+bool nextState[MAX_NUMCELLS];
 
 // ___________________________________________________________________________
 const int MAX_NUM_CELLS = 10'000;
 
-// ---------------------------------------------------------------------------
+// ___________________________________________________________________________
 void initTerminal() {
   initscr();
   cbreak();
@@ -19,4 +26,34 @@ void initTerminal() {
   mousemask(ALL_MOUSE_EVENTS, NULL);
   mouseinterval(0);
   start_color();
+
+  init_pair(1, COLOR_GREEN, COLOR_BLACK);
+  dimX = COLS / 2;
+  dimY = LINES;  
+}
+
+// ___________________________________________________________________________
+void initGame() {
+  posX = dimX / 2;
+  posY = dimY / 2;
+  StateGame = false;
+  numAliveCells = 0;
+}
+
+// ___________________________________________________________________________
+void ShowState() {}
+
+// ___________________________________________________________________________
+bool processUserInput(int keycode) {
+  if (keycode == KEY_MOUSE) {
+    MEVENT event;
+    if (getmouse(&event) == OK) {
+      if (event.bstate & BUTTON1_PRESSED) {
+        row = event.y;
+        col = event.x;
+        return true;
+      }
+    }
+  }
+  return false;
 }
--- blatt-04/GameOfLife.h
+++ blatt-04/GameOfLife.h
@@ -3,21 +3,29 @@
 #include <ncurses.h>
 #include <unistd>
 
+// Dimensions of the window.
+extern int numRows;
+extern int numCols;
+
+// Current position
+extern int row;
+extern int col;
+
 extern bool actualState[];
 extern bool nextState[];
 
 extern bool* a;
 extern bool* n;
 
-extern int x;
-extern int y;
+extern int posX;
+extern int posY;
 
 extern int numSteps;
 extern int numAliveCells;
 
 extern bool StateGame;
 
-// Initialize the terminal for use with ncurses commands.
+// Initializing the terminal and the field dimensions.
 void initTerminal();
 void initGame();
 
