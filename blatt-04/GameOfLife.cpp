32a565291923691d1cc4bda1c3d81d10bdb89637
student <erikhag04@gmail.com>
2026-05-19 09:48:36 +0200

    Changes

--- blatt-04/GameOfLife.cpp
+++ blatt-04/GameOfLife.cpp
@@ -1,5 +1,6 @@
 #include "GameOfLife.h"
 #include <ncurses.h>
+#include <stdlib>
 
 int numRows;
 int numCols;
@@ -53,7 +54,7 @@ void ShowState() {
   attron(COLOR_PAIR(1));
   for (int row = 0; row < numRows; ++row) {
     for (int col = 0; col < numCols; ++col) {
-      bool cellValue = actualState[row * numCols + col];
+      cellValue = actualState[row * numCols + col];
       if (cellValue) {
         attron(A_REVERSE);
       }
@@ -64,81 +65,93 @@ void ShowState() {
     }
   }
   attroff(COLOR_PAIR(1));
+  // brauche ich ein return ?
   return;
 }
 // ___________________________________________________________________________
 bool processUserInput(int keycode) {
   switch (keycode) {
-	case KEY_MOUSE: {
-    		MEVENT event;
-    		if (getmouse(&event) == OK) {
-      			if (event.bstate & BUTTON1_PRESSED) {
-        			row = event.y;
-        			col = event.x / 2;
-				a[row * numCols + col] = !a[row * numCols + col];
-				return true;
-			}
-		}
-		 
-        	break;
-	}
-	case ' ':
-		stateGame = !stateGame;
-		break;
-	case 'r':
-		for (int row = 0; row < numRows; ++row) {
-			for (int col = 0, col < numCols, ++col) {
-				if (rand() % 5 == 0) {
-					a[row * numCols + col] = true;
-				}
-				else {
-					a[row * numCols + col] = false;
-				}
-			}
-		}
-		break;
-	case 'q':
-		endwin();
-		printf("Game ended");
-	case 's':
-			
-
-// ___________________________________________________________________________
-int numAliveNeighbors(int row, int col) {
-  int aliveCount = 0;
-  for (int i = -1; i <= 1; ++i) {
-    for (int j = -1; j <= 1; ++j) {
-      int nRow = row + i;
-      int nCol = col + j;
-      if (i == 0 && j == 0) {
-        continue;
+  case KEY_MOUSE: {
+    MEVENT event;
+    if (getmouse(&event) == OK) {
+      if (event.bstate & BUTTON1_PRESSED) {
+        row = event.y;
+        col = event.x / 2;
+        a[row * numCols + col] = !a[row * numCols + col];
+        return true;
       }
-      if (nRow >= 0 && nRow < numRows && nCol >= 0 && nCol < numCols) {
-        if (a[nRow * numCols + nCol]) {
-          aliveCount += 1;
+    }
+  } break;
+  case ' ':
+    StateGame = !StateGame;
+    break;
+  case 'r':
+    for (int row = 0; row < numRows; ++row) {
+      for (int col = 0; col < numCols; ++col) {
+        if (rand() % 5 == 0) {
+          a[row * numCols + col] = true;
+        } else {
+          a[row * numCols + col] = false;
         }
-        continue;
       }
     }
-  }
-  return aliveCount;
-}
+    break;
+  case 'q':
+    endwin();
+    printf("Game ended");
+    return false;
+  case 's':
+    updateState();
+    return true;
+  case 'g': {
+        if ((row >= 0 && row + 2 < numRows) && (col >= 0 && col + 2 < numCols) {
+      a[row * numCols + col] = true;
+      a[row * numCols + (col + 2)] = true;
+      a[(row + 1) * numCols + (col + 1)] = true;
+      a[(row + 2) * numCols + col] = true;
+      a[(row + 2) * numCols + (col + 2)] = true;
+ 	}
+	else {
+      mvprintw("Error");
+	}
+	break;
 
-// ___________________________________________________________________________
-void updateState() {
-  for (int row = 0; row < numRows; ++row) {
-    for (int col = 0; col < numCols; ++col) {
-      int neighbors = numAliveNeighbors(row, col);
-      if (a[row * numCols + col] && (neighbors == 2 || neighbors == 3)) {
-        bool n[row * numCols + col] = true;
-      } else if (a[row * numCols + col] == false && neighbors == 3) {
-        n[row * numCols + col] = true;
-      } else {
-        n[row * numCols + col] = false;
+    // ___________________________________________________________________________
+    int numAliveNeighbors(int row, int col) {
+      int aliveCount = 0;
+      for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+          int nRow = row + i;
+          int nCol = col + j;
+          if (i == 0 && j == 0) {
+            continue;
+          }
+          if (nRow >= 0 && nRow < numRows && nCol >= 0 && nCol < numCols) {
+            if (a[nRow * numCols + nCol]) {
+              aliveCount += 1;
+            }
+            continue;
+          }
+        }
       }
+      return aliveCount;
     }
-  }
-  bool *tmp = a;
-  a = n;
-  n = tmp;
-}
+
+    // ___________________________________________________________________________
+    void updateState() {
+      for (int row = 0; row < numRows; ++row) {
+        for (int col = 0; col < numCols; ++col) {
+          int neighbors = numAliveNeighbors(row, col);
+          if (a[row * numCols + col] && (neighbors == 2 || neighbors == 3)) {
+            bool n[row * numCols + col] = true;
+          } else if (a[row * numCols + col] == false && neighbors == 3) {
+            n[row * numCols + col] = true;
+          } else {
+            n[row * numCols + col] = false;
+          }
+        }
+      }
+      bool *tmp = a;
+      a = n;
+      n = tmp;
+    }
