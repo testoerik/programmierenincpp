bfa75b715bc70d8550587a82aeda4d5c0f71c5e1
student <erikhag04@gmail.com>
2026-06-03 16:09:47 +0200

    Erste Ansätze

---
+++ blatt-06/String.cpp
@@ -0,0 +1,8 @@
+// Created by eh295
+#include "./String.h"
+
+void String::size() {
+  String *str = new String();
+  str = 0;
+  delete String;
+}
---
+++ blatt-06/String.h
@@ -0,0 +1,12 @@
+// Created by eh295
+
+class String {
+public:
+void size();
+
+private:
+};
+
+class StringSorter {
+  void sorter(int arg);
+};
