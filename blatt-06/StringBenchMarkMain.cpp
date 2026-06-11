3c9d2abc2b4d9f4f3263b45bf4f24955f10804ea
student <erikhag04@gmail.com>
2026-06-11 11:22:56 +0200

    .

--- blatt-06/StringBenchMarkMain.cpp
+++ blatt-06/StringBenchMarkMain.cpp
@@ -6,16 +6,25 @@ int main() {
   char basisNum = '1';
   int n = rand() % 6000;
 
-  String sorterCopy(n);
-  String sorterMove(n);
+  StringSorter sorterCopy(n);
+  StringSorter sorterMove(n);
   for (int i = 0; i < n; i++) {
     char tmpBuffer[101];
     for (int j = 0; j < 100; j++) {
       tmpBuffer[j] = basisNum + (rand() % 9);
     }
+    tmpBuffer[100] = '\0';
     sorterCopy[i] = tmpBuffer;
     sorterMove[i] = tmpBuffer;
   }
+  auto startCopy = clock();
   sorterCopy.sortWithCopy();
+  auto endCopy = clock();
+  double timeCopy = (endCopy - startCopy) / (double)CLOCKS_PER_SEC;
+  printf("Time for sorting %d objects with copy: %.2f seconds\n", n, timeCopy);
+  auto startMove = clock();
   sorterMove.sortWithMove();
+  auto endMove = clock();
+  double timeMove = (endMove - startMove) / (double)CLOCKS_PER_SEC;
+  printf("Time for sorting %d objects with move: %.2f seconds\n", n, timeMove);
 }
