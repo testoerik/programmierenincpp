542d78fcd9f6c1f304f5895c38b7f71d68954199
student <erikhag04@gmail.com>
2026-06-11 10:22:46 +0200

    Fixed StringBenchMarkMain

--- blatt-06/StringBenchMarkMain.cpp
+++ blatt-06/StringBenchMarkMain.cpp
@@ -1,16 +1,17 @@
-#include "./String.cpp"
+#include "./String.h"
 #include <cstdlib> // For random number.
 #include <ctime>   // For time measuring.
 
 int main() {
   char tmpBuffer[101];
+  char basisNum = '1';
   int n = rand() % 100;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < 100; j++) {
-      tempBuffer[i] = rand() % 10;
+      tmpBuffer[j] = basisNum + (rand() % 9);
     }
+    tmpBuffer[100] = '\0';
+    String tmp;
+    tmp = tmpBuffer;
   }
-  tmpBuffer[100] = '\0';
-  String tmp;
-  tmp = tmpBuffer;
 }
