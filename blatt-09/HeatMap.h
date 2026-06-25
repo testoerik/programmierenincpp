c7bb88f791b1f8deda586d9dc5729d61fb1420af
student <erikhag04@gmail.com>
2026-06-25 15:49:15 +0200

    .

--- blatt-09/HeatMap.h
+++ blatt-09/HeatMap.h
@@ -12,8 +12,21 @@ struct Point {
 struct Cell {
   int pixelRow_;
   int pixelCol_;
+  // Check if we have a duplicate of Cell object.
+  bool operator==(const Cell &other) {
+    return pixelRow_ = other.pixelRow_ && pixelCol_ == other.pixelCol_;
+  }
 };
 
+struct CellHash {
+  // Hashfunction as functor.
+  std::size_t operator()(const Cell &c) const {
+    std::size_t h1 = std::hash<int>{}(c.pixelRow_);
+    std::size_t h2 = std::hash<int>{}(c.pixelCol_);
+    // Avoid collision.
+    return h1 ^ (h2 << 1);
+  }
+};
 class HeatMap {
 public:
   // Getter function for vOfPoints_;
@@ -23,9 +36,9 @@ public:
   // Compute heatmap.
   void computeHeatMap(size_t numRows, size_t numCols, float aspectRatio);
   // Getter function which returns a reference to a unordered_map.
-  std::unordered_map<std::vector<Cell>, int> heatMap() { return m; }
+  std::unordered_map<Cell, int> heatMap() { return m; }
 
 private:
   std::vector<Point> vOfPoints_;
-  std::unordered_map<std::vector<Cell>, int> m;
+  std::unordered_map<Cell, int> m;
 };
