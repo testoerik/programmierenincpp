7b3220eec80cb9cbc79ad3bca121e826548860f8
student <erikhag04@gmail.com>
2026-07-17 18:24:17 +0200

    .

--- GeoGuesser/datacopy.cpp
+++ GeoGuesser/datacopy.cpp
@@ -1,9 +1,9 @@
+#include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 struct Point {
@@ -12,35 +12,46 @@ struct Point {
 };
 
 vector<Point> parseLine(const string &input) {
-  
+  vector<Point> v1;
+
   string objectName = input.substr(0, input.find('\t'));
   size_t start = input.find('\t') + 1;
-  string objectCoordinates = input.substr(start);
-  
-  size_t posOpenBracket = objectCoordinates.find_first_of('(');
-  objectCoordinates = objectCoordinates.substr(posOpenBracket);
-  cout << objectCoordinates;
-  size_t end = input.find_last_of('"');
-  replace(objectCoordinates.begin(), objectCoordinates.end(), '(', ' ');
-  replace(objectCoordinates.begin(), objectCoordinates.end(), ')', ' ');
-  replace(objectCoordinates.begin(), objectCoordinates.end(), ',', ' ');
-  float lon;
-  float lat;
-  vector<Point> v1;
-  cout << objectCoordinates << '\n';
-  stringstream coordinates(objectCoordinates);
-  while (coordinates >> lon >> lat) {
-	Point p1{lon, lat};
-	v1.push_back(p1);
+  if (start != string::npos) {
+    Point p1{0, 0};
+    v1.push_back(p1);
+    return v1;
+  } else {
+    string objectCoordinates = input.substr(start);
+
+    size_t posOpenBracket = objectCoordinates.find_first_of('(');
+    if (posOpenBracket != string::npos) {
+      Point p1{0, 0};
+      v1.push_back(p1);
+      return v1;
+    } else {
+      objectCoordinates = objectCoordinates.substr(posOpenBracket);
+      size_t end = input.find_last_of('"');
+      objectCoordinates = objectCoordinates.substr(start, end - start);
+      replace(objectCoordinates.begin(), objectCoordinates.end(), '(', ' ');
+      replace(objectCoordinates.begin(), objectCoordinates.end(), ')', ' ');
+      replace(objectCoordinates.begin(), objectCoordinates.end(), ',', ' ');
+      stringstream coordinates(objectCoordinates);
+      float lon;
+      float lat;
+      while (coordinates >> lon >> lat) {
+        Point p1{lon, lat};
+        v1.push_back(p1);
+      }
+      return v1;
+    }
   }
-  return v1;
 }
 
 int main() {
   string line;
   ifstream pointsFile("./tf-buildings.tsv");
   if (!pointsFile.is_open()) {
-	cout << "Fehler" << '\n';
+    cout << "Fehler" << '\n';
   }
   vector<vector<Point>> vectorPoints;
   cout << line;
@@ -48,8 +59,8 @@ int main() {
     vectorPoints.push_back(parseLine(line));
   }
   for (auto &vec : vectorPoints) {
-	for (auto &points : vec) { 
-		cout << points.longitude_ << '\n' << points.latitude_ << '\n';
-	}
+    for (auto &points : vec) {
+      cout << points.longitude_ << '\n' << points.latitude_ << '\n';
+    }
   }
 }
