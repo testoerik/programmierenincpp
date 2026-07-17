9ef71f9797db269df8aa71f6c64b8ddd6e78e746
student <erikhag04@gmail.com>
2026-07-17 18:09:39 +0200

    .

--- GeoGuesser/datacopy.cpp
+++ GeoGuesser/datacopy.cpp
@@ -14,14 +14,21 @@ struct Point {
 vector<Point> parseLine(const string &input) {
   
   string objectName = input.substr(0, input.find('\t'));
-  string objectCoordinates = input.substr(input.find('\t') + 1);
+  size_t start = input.find('\t') + 1;
+  string objectCoordinates = input.substr(start);
+  
+  size_t posOpenBracket = objectCoordinates.find_first_of('(');
+  objectCoordinates = objectCoordinates.substr(posOpenBracket);
+  cout << objectCoordinates;
+  size_t end = input.find_last_of('"');
   replace(objectCoordinates.begin(), objectCoordinates.end(), '(', ' ');
   replace(objectCoordinates.begin(), objectCoordinates.end(), ')', ' ');
-  replace(objectCoordinates.begin(), objectCoordinates.end(), ' ', ' ');
-  stringstream coordinates(objectCoordinates);
+  replace(objectCoordinates.begin(), objectCoordinates.end(), ',', ' ');
   float lon;
   float lat;
   vector<Point> v1;
+  cout << objectCoordinates << '\n';
+  stringstream coordinates(objectCoordinates);
   while (coordinates >> lon >> lat) {
 	Point p1{lon, lat};
 	v1.push_back(p1);
@@ -32,6 +39,9 @@ vector<Point> parseLine(const string &input) {
 int main() {
   string line;
   ifstream pointsFile("./tf-buildings.tsv");
+  if (!pointsFile.is_open()) {
+	cout << "Fehler" << '\n';
+  }
   vector<vector<Point>> vectorPoints;
   cout << line;
   while (getline(pointsFile, line)) {
